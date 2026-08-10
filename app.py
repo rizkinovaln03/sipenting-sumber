7bb19f52787026b5dd19a0270ace73f74e7b4381
rizkinovaln03 <riskifilla88@gmail.com>
2026-08-10 18:23:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -228,9 +228,12 @@ st.set_page_config(
 st.markdown("""
     <style>
     /* --- MENGHILANGKAN JEJAK STREAMLIT --- */
-    [data-testid="stHeader"] { visibility: hidden; }
-    #MainMenu { visibility: hidden; }
-    footer { visibility: hidden; }
+    /* Sembunyikan menu titik tiga dan logo GitHub di kanan atas */
+    [data-testid="stToolbar"] { visibility: hidden !important; }
+    /* Sembunyikan tulisan 'Made with Streamlit' di bawah */
+    footer { visibility: hidden !important; }
+    
+    /* CATATAN: Header atas sengaja TIDAK disembunyikan agar tombol panah menu (Sidebar) di HP tetap bisa ditekan! */
 
     /* CSS Desain Bawaanmu */
     .stApp { background-color: #F4F9F9; }
